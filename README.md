554447d1d3c577ff28d1df6e4008f37212f2936a
syssefim <serafim.sharkov@gmail.com>
2026-01-03 15:53:15 -0800

    Task Tracker CLI project is finished!

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# Task-Tracker-CLI
-https://roadmap.sh/projects/task-tracker
+# This is one of the beginner projects from the backend developer road from the website roadmap.sh. 
+
+
+
+💻 Installation \& Setup:
+
+* Clone the repository as so:
+
+&nbsp;	git clone https://github.com/syssefim/Task-Tracker-CLI
+
+&nbsp;	cd task-tracker-cli
+
+* In the terminal navigate to the folder containing the project
+* Now you're ready for the 📖 Usage Guide
+
+
+
+
+
+
+
+
+
+
+
+📖 Usage Guide
+
+1. Add a task:
+   	To add a task write:
+   		python task\_tracker\_cli.py add "Buy groceries"
+2. List all tasks or filter by status (todo, in-progress, done):
+   	To either list all tasks or list tasks by their status write:
+   		# List all
+   		python task\_tracker\_cli.py list
+
+   		# List by status
+   		python task\_tracker\_cli.py list done
+   		python task\_tracker\_cli.py list todo
+3. Update a task:
+   	To change the description of a task given it's id write:
+   		# Usage: update <id> <new\_description>
+   		python task\_tracker\_cli.py update 1 "Buy groceries and cook dinner"
+4. Delete a task:
+   	To delete a task given it's id write:
+   		# Usage: delete <id>
+   		python task\_tracker\_cli.py delete 1
+5. Chance task status:
+
+&nbsp;	You can chance the status of any given task either to "in progress" or "done". To do so write:
+
+&nbsp;		# Mark as In Progress
+
+&nbsp;		python task\_tracker\_cli.py mark-in-progress 1
+
+
+
+&nbsp;		# Mark as Done
+
+&nbsp;		python task\_tracker\_cli.py mark-done 1
+
--- task_tracker_cli.py
+++ task_tracker_cli.py
@@ -12,12 +12,7 @@ TASK_FILE = 'tasks.json'
 
 
 
-
-
-
-
-
-
+#main function that appropriately calls the correct function based on user input
 def main():
     if len(sys.argv) < 2:
             print("Usage: task-cli <command> [arguments]")
@@ -31,15 +26,31 @@ def main():
         else:
             add_task(sys.argv[2])
     elif command == 'update':
-        print('registered update')
+        if len(sys.argv) < 3:
+                print("Error: Missing task description.")
+        else:
+            print(sys.argv[2])
+            update_task(sys.argv[2], sys.argv[3])
     elif command == 'delete':
-        print('registed delete')
+        if len(sys.argv) < 2:
+                print("Error: Missing task ID.")
+        else:
+            delete_task(sys.argv[2])
     elif command == 'mark-in-progress':
-        print('registed mark-in-progress')
+        if len(sys.argv) < 2:
+                print("Error: Missing task ID.")
+        else:
+            mark_in_progress_task(sys.argv[2])
     elif command == 'mark-done':
-        print('registered mark-done')
+        if len(sys.argv) < 2:
+                print("Error: Missing task ID.")
+        else:
+            mark_done_task(sys.argv[2])
     elif command == 'list':
-        print ('registered list')
+        if len(sys.argv) <= 2:
+                list_tasks()
+        else:
+            list_tasks(sys.argv[2])
     else:
         print ("command not recognized...")
 
@@ -66,7 +77,7 @@ def load_tasks():
     except (json.JSONDecodeError, IOError):
         return []
 
-
+#saves data to tasks.json and then closes it
 def save_tasks(tasks):
     try:
         with open(TASK_FILE, 'w') as file:
@@ -77,7 +88,7 @@ def save_tasks(tasks):
 
 
 
-
+#function that adds tasks to tasks.json
 def add_task(description):
     tasks = load_tasks()
 
@@ -95,6 +106,74 @@ def add_task(description):
     save_tasks(tasks)
     print(f"Task added successfully (ID: {new_id})")
                      
+
+#function that changes the task description of a task given it's id
+def update_task(task_id, new_description):
+     tasks = load_tasks()
+     for task in tasks:
+        if task['id'] == int(task_id): 
+            task['description'] = new_description
+            task['updatedAt'] = datetime.datetime.now().isoformat()
+            save_tasks(tasks)
+            print(f"Task {task_id} updated successfully.")
+            return
+     print(f"Task with ID {task_id} not found.")
+
+
+#function that deletes a task given it's id
+def delete_task(task_id):
+    tasks = load_tasks()
+    new_tasks = [t for t in tasks if t['id'] != int(task_id)]
+    
+    if len(tasks) == len(new_tasks):
+        print(f"Task with ID {task_id} not found.")
+    else:
+        save_tasks(new_tasks)
+        print(f"Task {task_id} deleted successfully.")
+
+#function that marks a task's status as inprogress given it's id
+def mark_in_progress_task(task_id):
+    tasks = load_tasks()
+
+    for task in tasks:
+        if task['id'] == int(task_id): 
+            task['status'] = "inprogress"
+            task['updatedAt'] = datetime.datetime.now().isoformat()
+            save_tasks(tasks)
+            print(f"Task {task_id} updated successfully.")
+            return
+    print(f"Task with ID {task_id} not found.")
+
+#function that marks a task's status as done given it's id
+def mark_done_task(task_id):
+     tasks = load_tasks()
+
+     for task in tasks:
+        if task['id'] == int(task_id): 
+            task['status'] = "done"
+            task['updatedAt'] = datetime.datetime.now().isoformat()
+            save_tasks(tasks)
+            print(f"Task {task_id} updated successfully.")
+            return
+     print(f"Task with ID {task_id} not found.")
+
+#function that lists all tasks, optionally filtering by status
+def list_tasks(status_filter=None):
+    tasks = load_tasks()
+    
+    if not tasks:
+        print("No tasks found.")
+        return
+
+    for task in tasks:
+        # If a filter is provided (e.g., 'done'), skip tasks that don't match
+        if status_filter and task['status'] != status_filter:
+            continue
+            
+        print(f"[{task['id']}] {task['description']} - {task['status']}")
+
+
+
 
 
 
